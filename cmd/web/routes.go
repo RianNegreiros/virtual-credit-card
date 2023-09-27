6da2669c65a0834b34f42f333ecd364660247afb
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-09-27 13:49:05 -0300

    feat: add basic handler

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -9,7 +9,7 @@ import (
 func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
 
-	mux.Get("/virtual-terminal", app.virtualTerminal)
+	mux.Get("/virtual-terminal", app.VirtualTerminal)
 
 	return mux
 }
